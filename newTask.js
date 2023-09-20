3c3a76a916c05f8a8efc9b13d585d68eff1538d2
Artyom Harutyunyan <ka6rias@gmail.com>
2023-09-21 00:26:38 +0400

    some fixes

--- newTask.js
+++ newTask.js
@@ -8,51 +8,50 @@ function checkAmount(amount) {
 
 class Account {
   #balance = 0;
+
   constructor(name) {
     this.name = name;
     this.id = createUniqId();
   }
+
   get balance() {
     return this.#balance;
   }
+  
   set balance(newBalance) {
     if (checkAmount(newBalance))
       this.#balance = newBalance
-    else throw new Error("Wrong amount");
+    else throw new Error("Wrong balance");
   }
+
   credit(increaseAmount) {
     if (checkAmount(increaseAmount))
       this.#balance += increaseAmount
     else throw new Error("Wrong amount");
   }
+
   debit(decreaseAmount) {
     if (checkAmount(decreaseAmount) && decreaseAmount <= this.#balance)
       this.#balance -= decreaseAmount;
-    else throw new Error("Insufficient funds");
+    else throw new Error("Wrong amount");
   }
+
   transferTo(anotherAcc, amount) {
     if (checkAmount(amount) && amount <= this.#balance) {
       this.#balance -= amount;
       anotherAcc.balance += amount;
     }
-    else
-      throw new Error("Insufficient funds transfer");
+    else throw new Error("Wrong amount for transfer");
   }
+
   static identifyAccounts(...accounts) {
-    const ids = {}
-    for (const id of accounts) {
-      if (id in ids)
+    const idsSet = new Set()
+    for (const account of accounts) {
+      if (idsSet.has(account.id))
         return false
-      ids[id] = true;
+      idsSet.add(account.id);
     }
+    return [true, idsSet]
   }
 }
 
-const first = new Account('Tyom')
-const second = new Account('Armin')
-
-first.credit(5000)
-second.credit(5000)
-
-console.log(first.balance);
-console.log(second.balance);
