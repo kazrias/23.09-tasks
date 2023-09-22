3b4fdc31e9d7fa695b4b804761a3b6c1c9c06ef6
Artyom Harutyunyan <ka6rias@gmail.com>
2023-09-22 19:09:35 +0400

    jungle and animal class finish

---
+++ newTask.js
@@ -0,0 +1,93 @@
+class Jungle {
+  constructor() {
+    this.tigers = []
+    this.monkeys = []
+    this.snakes = []
+  }
+  soundOff() {
+
+  }
+
+}
+
+class Animal {
+  #energy = 10;
+  constructor(jungle) {
+    if (this instanceof Tiger)
+      jungle.tigers.push(this)
+    if (this instanceof Monkey)
+      jungle.monkeys.push(this)
+    if (this instanceof Snake)
+      jungle.snakes.push(this)
+    else return 'There is no place for such animal in this jungle'
+  }
+  get energy() {
+    return this.#energy
+  }
+  set energy(val) {
+    if (val < 0)
+      console.error(`${this.type} doesn't have enough energy`)
+    else this.#energy = val;
+
+  }
+  sound() {
+    this.energy -= 3;
+  }
+  eat(food) {
+    this.energy += 5;
+  }
+  sleep() {
+    this.energy += 10;
+  }
+
+}
+
+class Food {
+
+}
+
+class Tiger extends Animal {
+  constructor(jungle) {
+    super(jungle);
+    this.type = 'tiger'
+  }
+  sleep() {
+    this.energy += 5;
+  }
+}
+
+class Monkey extends Animal {
+  constructor(jungle) {
+    super(jungle);
+    this.type = 'monkey'
+  }
+  play() {
+    if (this.energy >= 8) {
+      this.energy -= 8;
+      console.log('“Oooo Oooo”');
+    }
+    else console.log('“I’m too tired”')
+  }
+}
+
+class Snake extends Animal {
+  constructor() {
+    super();
+    this.type = 'snake'
+  }
+}
+
+class Fish extends Food {
+
+}
+
+class Grain extends Food {
+
+}
+
+class Meat extends Food {
+
+}
+
+
+
